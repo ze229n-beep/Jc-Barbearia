9878c79a666e479de9417b9213408b75fcd11927
ze229n-beep <ze229n@gmail.com>
2026-09-09 21:20:54 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,8 @@
+const SUPABASE_URL = "https://zlfkdyxlfegojlgzulue.supabase.co";
 
+const SUPABASE_ANON_KEY = "sb_publishable_suqAkNXSnGacL1vO_BaaOg_oeeHDKSs";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
